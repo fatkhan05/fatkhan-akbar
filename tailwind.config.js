0d70935357df7278f4e288304762f2200e386bc3
fatkhan05 <watsittoya469@gmail.com>
2025-02-18 11:53:32 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '2px',
+    },
+    extend: {
+      colors:{
+        dark: '#0f172a',
+        primary: '#1FC2F0',
+        secondary: 'rgb(55 88 120)',
+        donker: '#192d3e',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
